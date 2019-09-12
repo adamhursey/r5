b8015be16a434069ad7c9f393243efdc086c89d7
Adam Hursey <adamhursey24@gmail.com>
2019-09-11 18:03:00 -0700

    removed link on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import '../css/Header.css';
 
 const Header = () => (
   <div className="Header-main">
-    <Link to="/home">
-      <div className="Title">
-        <h1>R5</h1>
-      </div>
-    </Link>
+    <div className="Title">
+      <h1>R5</h1>
+    </div>
   </div>
 );
 
